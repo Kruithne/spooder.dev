9e254b74f1afc7f46526967e94f67bfd09a58760
Kruithne <kruithne@gmail.com>
2023-09-08 10:58:58 +0100

    add spooder update endpoint

--- index.ts
+++ index.ts
@@ -7,6 +7,14 @@ server.error((err: Error) => {
 	return new Response('The kākāpō has exploded (internal server error)', { status: 500 });
 });
 
+server.route('/internal/update/:key', (req: Request, url: URL) => {
+	if (url.searchParams.get('key') !== process.env.SPOODER_UPDATE_KEY)
+		return 401; // unauthorized
+
+	server.stop(ServerStop.GRACEFUL);
+	return 200; // ok
+});
+
 server.route('/test_response_code', (req: Request, url: URL) => {
 	return 418; // I'm a teapot
 });
